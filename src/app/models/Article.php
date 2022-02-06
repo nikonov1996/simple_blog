2da29dcd1dc3c38548ccaeb452974db19759f6ed
dev <dev@developer-001>
2022-02-06 23:42:13 +0700

    added article create method in model

--- src/app/models/Article.php
+++ src/app/models/Article.php
@@ -39,7 +39,8 @@ class Article extends AbstractArticle{
         $article_description = $formdata['article_description'];
         $article_text = $formdata['article_text'];
         $author_id = $formdata['author_id'];
-        
+        $sql = 'INSERT INTO Articles(article_name, article_description, article_text,author_id)
+        VALUES ("' . $article_title . '","' . $article_description . '","' . $article_text . '","' . $author_id . '");';
         $db = new ConnectionDB();
         $db->sqlMake("db", $sql);
     }
